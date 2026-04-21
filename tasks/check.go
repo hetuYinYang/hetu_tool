71ee1cd56aaf250947ae26bad430109fdb0bbed7
孟辉辉 <296834540@qq.com>
2026-04-21 18:04:18 +0800

    feat: 新增数据校验逻辑

--- tasks/check.go
+++ tasks/check.go
@@ -10,3 +10,31 @@ func TaskRunning(taskId string) bool {
 	}
 	return true
 }
+func TaskStopped(taskId string) bool {
+	if taskId == "" {
+		return false
+	}
+	return true
+}
+func IsReRunning(taskId string) bool {
+	if taskId == "" {
+		return false
+	}
+	return true
+}
+func CheckList[T any](dataList []T) bool {
+	if len(dataList) == 0 {
+		return false
+	}
+	return true
+}
+
+func CheckNum(str string) bool {
+	if str == "" {
+		return false
+	}
+	return true
+}
+func Is() {
+
+}
